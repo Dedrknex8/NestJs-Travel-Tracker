3174e2856087160fa34f07eda6e6af31327a1d4d
Rohit Tiwari <166986989+Dedrknex8@users.noreply.github.com>
2025-03-19 14:47:35 +0530

    Added login function with jwtToken

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Registerdto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
 
 //think it as routes and service as controller
 @Controller('auth')
@@ -10,5 +11,9 @@ export class AuthController {
     @Post('regiser')
     async regiser(@Body() registerDto: Registerdto){
         return this.authService.register(registerDto)
+    }
+    @Post('login')
+    async login(@Body() loginDto:LoginDto){
+        return this.authService.login(loginDto);
     }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,16 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { Registerdto } from './dto/register.dto';
 import * as bcrypt from 'bcryptjs'
 import { log } from 'node:console';
+import { LoginDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
+import { resourceLimits } from 'node:worker_threads';
 @Injectable()
 export class AuthService {
     constructor (
-        private prisma : PrismaService
+        private prisma : PrismaService,
+        private jwtService : JwtService
     ){} 
     //handle new user registration
     async register(registerDto: Registerdto){
@@ -32,5 +36,31 @@ export class AuthService {
         //remove passwd form return object
         const {password: _, ...result} = newnlyCreatedUser
         return result;
-}   
+    }  
+    
+    async login(loginDto : LoginDto){
+        const {email,password} = loginDto;
+        //first find the user with the unique email id
+
+        const user = await this.prisma.user.findUnique({
+            where : { email }
+        });
+
+        if(!user){
+            throw new UnauthorizedException('Invalid credential. Please try again ! ');
+        }
+
+        const isPassword = await bcrypt.compare(password,user.password);
+        
+        if(!isPassword){
+            throw new UnauthorizedException('Invalid credential. Please try again ! ');
+        }
+
+        //genearte a token
+        const token = this.jwtService.sign({userId : user.id});
+
+        const {password:_,...result} = user;
+
+        return { ...result,token};
+    }
 }
